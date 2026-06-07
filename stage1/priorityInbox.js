cada3cad0626c0b26e864dadbc3ac852d4279904
swetha1772 <swethapalepu47@gmail.com>
2026-06-07 12:21:57 +0530

    added API sc and final stage 1

--- stage1/priorityInbox.js
+++ stage1/priorityInbox.js
@@ -1,4 +1,6 @@
 const logger = require("../loggingMiddleware");
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJwc3dldGhhQGdpdGFtLmluIiwiZXhwIjoxNzgwODEyNDEwLCJpYXQiOjE3ODA4MTE1MTAsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiIyNGEyZWRhOS02NmQ0LTRhZTItYmM3Ni1hNTU1OTRlZGRhN2MiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJwYWxlcHUgc3dldGhhIiwic3ViIjoiYzhhYTM5OWQtODg1ZS00MjM0LThiZGQtMDI4YTRiMzgxMTU4In0sImVtYWlsIjoicHN3ZXRoYUBnaXRhbS5pbiIsIm5hbWUiOiJwYWxlcHUgc3dldGhhIiwicm9sbE5vIjoiMjAyMzAwNjY3NiIsImFjY2Vzc0NvZGUiOiJ3Z0t0Z1oiLCJjbGllbnRJRCI6ImM4YWEzOTlkLTg4NWUtNDIzNC04YmRkLTAyOGE0YjM4MTE1OCIsImNsaWVudFNlY3JldCI6IktHWWRIbkd2emdYdlpCY3gifQ.6DlRWELHMdeRiEinyFVvTVwQ3V9HRIQh7GMa8IsWcZc";
+
 
 const priorityMap = {
   Placement: 3,
@@ -6,47 +8,66 @@ const priorityMap = {
   Event: 1
 };
 
+function getScore(notification) {
+  const weight = priorityMap[notification.Type];
+  const time = new Date(notification.Timestamp).getTime();
+
+  return weight * 1000000000000 + time;
+}
+
+// Fetch notifications from API
+async function fetchNotifications() {
+  logger("Fetching notifications from API");
+
+  try {
+    const response = await fetch("http://4.224.186.213/evaluation-service/notifications", {
+      method: "GET",
+      headers: {
+        "Authorization":'Bearer ${TOKEN}'
+      }
+    });
+
+    if (!response.ok) {
+      logger("API error", response.status);
+      return [];
+    }
+
+    const data = await response.json();
+
+    logger("API response received");
+
+    return data.notifications || [];
+  } catch (error) {
+    logger("Error fetching notifications", error);
+    return [];
+  }
+}
+
 function getTopNotifications(data, n = 10) {
   logger("Starting priority calculation");
 
-  logger("Sorting notifications");
-
   const result = data
-    .sort((a, b) => {
-      if (priorityMap[b.Type] !== priorityMap[a.Type]) {
-        return priorityMap[b.Type] - priorityMap[a.Type];
-      }
-      return new Date(b.Timestamp) - new Date(a.Timestamp);
-    })
+    .sort((a, b) => getScore(b) - getScore(a))
     .slice(0, n);
 
-  logger("Top notifications generated", result);
+  logger("Top notifications generated", JSON.stringify(result, null, 2));
 
   return result;
 }
 
-// Sample data
-const notifications = [
-  {
-    ID: "1",
-    Type: "Result",
-    Message: "project-review",
-    Timestamp: "2026-04-22 17:50:42"
-  },
-  {
-    ID: "2",
-    Type: "Event",
-    Message: "tech-fest",
-    Timestamp: "2026-04-22 17:50:06"
-  },
-  {
-    ID: "3",
-    Type: "Placement",
-    Message: "interview-call",
-    Timestamp: "2026-04-22 17:51:00"
+// Main function
+async function main() {
+  const notifications = await fetchNotifications();
+
+  if (notifications.length === 0) {
+    logger("No notifications found");
+    return;
   }
-];
 
-const top = getTopNotifications(notifications);
+  const top = getTopNotifications(notifications);
 
-console.log(top); // okay to show output
+  console.log("\nTop Notifications:\n");
+  console.log(top);
+}
+
+main();
